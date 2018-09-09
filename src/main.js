d6751b389330812ea6a39c63ea246b118bfc153c
Mark Mahoney <mrkmhny@users.noreply.github.com>
2018-09-09 01:44:57 -0400

    Connect socket.io and vuex store through main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
+import io from 'socket.io-client';
+import VueSocketio from 'vue-socket.io';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 import './registerServiceWorker';
 
+const socket = io.connect('http://localhost:3000');
+Vue.use(VueSocketio, socket);
+Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
