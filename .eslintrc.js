a428cdbebe24f1eea3c2c9339f91f7ec455c3d73
Mark Mahoney <mrkmhny@users.noreply.github.com>
2018-09-09 01:36:24 -0400

    Modify eslint config to allow for function argument property modifications in vuex and socket.io

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,21 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'acc', // for reduce accumulators
+        'e', // for e.returnvalue
+        'ctx', // for Koa routing
+        'req', // for Express requests
+        'request', // for Express requests
+        'res', // for Express responses
+        'response', // for Express responses
+        'state', // for Vuex mutations
+        'socket', // for Socket.io
+        '$scope', // for Angular 1 scopes
+      ]
+    }],
   },
   parserOptions: {
     parser: 'babel-eslint',
