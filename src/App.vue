8cdd963eefc04c4f0134d2c7cc81e3c5986b7c50
Mark Mahoney <mrkmhny@users.noreply.github.com>
2018-09-09 01:41:16 -0400

    Add first socket.io reactions to parent vue component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,67 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <!-- <router-link to="/about">About</router-link> -->
+    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  #app {
+    min-height: 100vh;
+    // & > div {
+    //   min-height: 100vh;
+    // }
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  }
+  #nav {
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
+
+<script>
+import io from 'socket.io-client';
+export default {
+  name: 'app',
+  sockets: {
+    connect: function() {
+      console.log('Connected to socket.io');
+      // TODO: Save socket id in store to identify which socket is you.
+    },
+    selfJoinedRoom: function(res) {
+      console.log(res.msg);
+      this.$router.push({name: 'room', params: { roomId: res.roomId }})
+    },
+    playerJoinedRoom: function(res) {
+      console.log('playerJoinedRoom', res);
+      this.$store.commit('setPlayers', res.roomPlayers);
+    },
+    playerLeftRoom: function userLeftRoom(res) {
+      console.log(res.msg);
+    }
+  },
+}
+</script>
+
