a91b9cccf02e1453c671c956adcf38b0e077cb47
Mark Mahoney <mrkmhny@users.noreply.github.com>
2018-09-09 01:46:08 -0400

    Add new route for /room

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 // import Home from './views/Home.vue';
-import Entrance from './views/Entrance.vue'
+import Entrance from './views/Entrance.vue';
 
 Vue.use(Router);
 
@@ -15,12 +15,12 @@ export default new Router({
       component: Entrance,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/room/:roomId',
+      name: 'room',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './views/Room.vue'),
     },
   ],
 });
